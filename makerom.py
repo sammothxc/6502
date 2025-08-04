8133a5ed62baf120dd1fd7bfa6e2b64fcd41facf
Sam Warr <sam.warr.tx@gmail.com>
2025-08-04 00:00:12 -0600

    rom2

--- makerom.py
+++ makerom.py
@@ -11,7 +11,7 @@ code = bytearray([
     0x4c, 0x05, 0x80   # JMP $8005
 ])
 
-rom = bytearray([0xea] * 32768 - (len(code))) # Fill with NOPs
+rom = code + bytearray([0xea] * (32768 - len(code))) # Fill with NOPs
 
 rom[0x7ffc] = 0x00  # Set the reset vector to point to the start of the code
 rom[0x7ffd] = 0x80
