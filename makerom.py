4f6e159b4c4006011e6ad147d95b593f9b7be608
Sam Warr <sam.warr.tx@gmail.com>
2025-08-03 23:51:54 -0600

    rom2

--- makerom.py
+++ makerom.py
@@ -1,5 +1,13 @@
-rom = bytearray([0xea] * 32768)  # 32KB ROM filled with NOPs
+code = bytearray([
+    0xa9, 0x42,  # LDA #$42
+    0x8d, 0x00, 0x60,  # STA $6000
+])
 
-with open("rom.bin", "wb") as out_file:
+rom = bytearray([0xea] * 32768 - len(code))
+
+rom[0x7ffc] = 0x00  # Set the reset vector to point to the start of the code
+rom[0x7ffd] = 0x80
+
+with open("rom2.bin", "wb") as out_file:
     out_file.write(rom)
-print("ROM created with 32KB of NOPs.")
+print("ROM created")
